1fe1b64da5010d694467c7f2a76ac72af4ecba91
Shikhar Walia <shikhar.walia@hcl.com>
2022-11-23 12:56:55 +0530

    I hope this will commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
        <h1>Introduction</h1>
        <p>push command : git push --set-upstream origin master</p>
+       <h2>Hello Branch</h2>
       <p>Hello pull </p>
     </div>
   );
