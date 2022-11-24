902061f0f10ab37793487c2819c0981b5f15b3f4
shikhar walia <shikhar.walia@hcl.com>
2022-11-24 10:30:07 +0530

    changes done in master branch

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
        <h1>Introduction</h1>
        <p>push command : git push --set-upstream origin master</p>
        <h2>Hello Branch</h2>
+       <div>This is to check for master branch</div>
       <p>Hello pull </p>
     </div>
   );
