dbe2a195a26ea28a433b93b1ee04eea2e8277818
shikhar walia <shikhar.walia@hcl.com>
2022-11-25 12:01:32 +0530

    changes done

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,12 @@ function App() {
     <div className="App">
        <h1>Introduction</h1>
        <p>push command : git push --set-upstream origin master</p>
-<<<<<<< HEAD
        <h2>Hello Branch</h2>
        <div>This is to check for master branch</div>
-=======
->>>>>>> feature/BETA
       <p>Hello pull </p>
       <h5>jhvcduv</h5>
       <div>
-        hello this is our main page
+        hello this is our main page for master
       </div>
     </div>
   );
